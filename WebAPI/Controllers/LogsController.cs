f3f25eac792385ae8ad7e132c7e001f12625eadf
Andrey Starostenkov <kinwend17@gmail.com>
2022-12-25 06:19:34 +0300

    upds

---
+++ WebAPI/Controllers/LogsController.cs
@@ -0,0 +1,43 @@
+﻿using Application.DTO.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class LogsController : ControllerBase
+{
+    private ILogger<LogsController> _logger;
+
+    public LogsController(ILogger<LogsController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpPost]
+    public IActionResult Log(LogMessage msg)
+    {
+        switch (msg.Level)
+        {
+            case 0:
+                _logger.Log(LogLevel.Trace, $"[ANG]: {msg.Message}");
+                break;
+            case 1:
+                _logger.Log(LogLevel.Debug, $"[ANG]: {msg.Message}");
+                break;
+            case 2:
+                _logger.Log(LogLevel.Information, $"[ANG]: {msg.Message}");
+                break;
+            case 3:
+                _logger.Log(LogLevel.Information, $"[ANG]: {msg.Message}");
+                break;
+            case 4:
+                _logger.Log(LogLevel.Warning, $"[ANG]: {msg.Message}");
+                break;
+            case 5:
+                _logger.Log(LogLevel.Error, $"[ANG]: {msg.Message}");
+                break;
+            default:
+                _logger.Log(LogLevel.None, $"[ANG]: {msg.Message}");
+                break;
+        }
+        return Ok();
+    }
+}
